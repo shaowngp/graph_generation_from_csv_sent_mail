322c8871266fe8dc1ae6f017da47d7248603f734
shaowngp <shaowngp@outlook.com>
2020-03-27 23:04:57 +0600

    Added all the required fpython script with dependencies.

---
+++ csv_graph_image.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Imam Uddin Ahamed
+
+# Importing required libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.style 
+from datetime import datetime
+import runpy
+import csv
+
+# Storing pursed datetime function into variable 
+now = datetime.now()
+dateinfo = now.strftime("%m%d%Y")
+print("date and time:",dateinfo)
+
+# Reading the CSV file & storing in a variable
+a=pd.read_csv('D:\\Python_test\\tps_report.csv')  #--> sample file path example in windows  
+
+
+# Creating an image dynamic image dile name based on date
+file_name='D:\\Python_test\\daily_avg_tps_'+dateinfo+'.png'
+print(file_name)
+
+# Setting graph plot style
+plt.style.use('ggplot')
+
+
+# Setting Column names in a variable
+A=a['Day']
+B=a['Avg_TPS']
+
+
+# Setting plot figure Heading
+fig = plt.figure()
+fig.suptitle('Daily Average TPS')
+
+# Setting X & Y axis label with desired values
+plt.xlabel('DATE')
+plt.ylabel('AVG_TPS')
+
+# Plotting the graph & save the graph in an image
+plt.plot(A,B)
+plt.savefig(file_name)
+
+# Calling a python script required for sending mails with 
+runpy.run_path(path_name='sending_mails.py')
+
+print("Mail has been sent with attachments. Thanks !!!");
+# Thanks for viewing the script. If you want to add more advance features , please comment on github.
---
+++ sending_mails.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+#Author: Imam Uddin Ahamed
+
+
+# Sending mails to multiple recipients with attachment from Google Account which can be replaced by your organization / other smtp config. 
+
+  
+# Required libraries to import 
+import smtplib 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email.mime.multipart import MIMEMultipart 
+from email import encoders 
+
+# Creating function/method to send mails with attachment 
+def send_mail():
+    # Mail sending from 
+    fromaddr = "xyz@gmail.com"
+
+    # Mail recipients 
+    toaddr = [ "xyz@outlook.com","abc@gmail.com "] 
+
+    # Mail cc recipients
+    ccaddr = ["mnp@outlook.com"]
+
+    # Message in MIMEMultipart 
+    msg = MIMEMultipart() 
+
+    # storing the senders email address   
+    msg['From'] = fromaddr 
+
+    # storing the recipients email address  
+    msg['To'] = ", ".join(toaddr)
+
+    msg['Cc'] = ", ".join(ccaddr)
+
+    toaddr.extend(ccaddr)
+
+    # storing the subject  
+    msg['Subject'] = "Test: Daily Average TPS"
+
+    # string to store the body of the mail 
+    body = "Hi, This is Test."
+
+    # attach the body with the msg instance 
+    msg.attach(MIMEText(body, 'plain')) 
+
+    # Storing file name in a variable to be sent  
+    filename = "daily_avg_tps_03272020.png"
+    
+    # Opening required attachment file to be sent  
+    attachment = open("D:\\Python_test\\daily_avg_tps_03272020.png", "rb") 
+
+    # Instance of MIMEBase and named as appmime 
+    appmime = MIMEBase('application', 'octet-stream') 
+
+    # Changing the payload into encoded form 
+    appmime.set_payload((attachment).read()) 
+
+    # Encoding into base64 
+    encoders.encode_base64(appmime) 
+
+    appmime.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+
+    # Attaching the instance 'appmime' to instance 'msg' 
+    msg.attach(appmime) 
+
+    # Creating Google's SMTP session - for TLS : 587 , for ssl : 465 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+
+    # Starting TLS for security 
+    s.starttls() 
+
+    print("Before Login to my mail account");  
+
+    # Logging in using my email credentials
+
+    s.login(fromaddr, "XXXXXXX")  #--> Here "XXXXXX" represent your password. You can make it as property base.
+
+    print("After Login to my mail account");  
+
+    # Converts the Multipart msg into a string 
+    text = msg.as_string() 
+
+    
+    # Sending the mail with attachment
+    # Important: Beforesending mails, you have to enable "Allow less secure apps: ON" in your google account.
+    # Ref. to enable "Allow less secure apps": https://myaccount.google.com/u/1/lesssecureapps?pageId=none
+
+    s.sendmail(fromaddr,toaddr,text) 
+
+
+    # Closing the session 
+    s.quit() 
+
+
+send_mail()
+
+# For any queries & help, please let me know. mail id: shaowngp@outlook.com.
+# I have taken helps from Various group & made customized as per my requirement. Hope it'll benefit you as well. :-)
+
